f9c24d08e52d8d4e0d0d0ffd81397b8fb4f36075
shoshana2133 <s0527183541@gmail.com>
2023-01-01 13:14:31 +0200

    Letter pair function

---
+++ frequency/LetterPairs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace frequency
+{
+    class LetterPairs
+    {
+        static Dictionary<string, int> Dic_LetterPairs;
+        static public void FillingDic(string text)
+        {
+            for (int i = 0; i < text.Length-1; i++)
+            {
+                Dic_LetterPairs.TryAdd(text[i] + text[i + 1], 1);
+            }
+        }
+    }
+}
