adf59c344c2bfe1f1e1277354d648b38d3548e54
shoshana2133 <s0527183541@gmail.com>
2023-01-08 16:51:48 +0200

    A function that builds a dictionary of pairs of letters and counters

--- frequency/LetterPairs.cs
+++ frequency/LetterPairs.cs
@@ -1,18 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace frequency
 {
     class LetterPairs
     {
-        static Dictionary<string, int> Dic_LetterPairs;
+        static Dictionary<string, int> Dic_LetterPairs=new Dictionary<string, int>();
+        static Dictionary<string, int> bla = new Dictionary<string, int>();
+
+
+        //פונקציה שמחלקת את הטקסט לזוגות של אותיות ומכניסה אותם למילון עם מונה הופעות של כל זוג
         static public void FillingDic(string text)
         {
-            for (int i = 0; i < text.Length-1; i++)
+            for (int i = 1; i < text.Length-1; i++)
             {
-                Dic_LetterPairs.TryAdd(text[i] + text[i + 1], 1);
+                if (text[i] + "" == " " || text[i + 1] + "" == " ")
+                    continue;
+                    if(Dic_LetterPairs.ContainsKey(text[i] + "" + text[i + 1]))
+                    Dic_LetterPairs[text[i] +""+ text[i + 1]] ++;
+                    else
+                    Dic_LetterPairs.Add(text[i] + "" + text[i + 1], 1);
             }
+               Dic_LetterPairs.Max(x => x.Value);
         }
     }
 }
